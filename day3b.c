17b1dac43aa72c16fbd4aa4efa762d34394885e1
zinnysood14-lgtm <zinnysood14@gmail.com>
2026-08-19 00:37:34 +0530

    Add files via upload

---
+++ day3b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}
