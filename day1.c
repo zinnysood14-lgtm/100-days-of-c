9189dd5ed2434fdaac34972b0522d5aabf27f7fe
zinnysood14-lgtm <zinnysood14@gmail.com>
2026-08-19 00:07:38 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float a, b;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    printf("Sum = %.2f\n", a + b);
+    printf("Difference = %.2f\n", a - b);
+    printf("Product = %.2f\n", a * b);
+
+    if (b != 0)
+        printf("Quotient = %.2f\n", a / b);
+    else
+        printf("Division by zero is not possible.\n");
+    return 0;
+}
