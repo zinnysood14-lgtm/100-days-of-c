32edc9e6f684ef701701337a62cc8045c4460fec
zinnysood14-lgtm <zinnysood14@gmail.com>
2026-08-20 00:42:19 +0530

    Add files via upload

---
+++ day4b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, sum=0;
+printf("Enter a natural number: ");
+scanf("%d", &n);
+
+for (int i=1; i<=n; ++i)
+sum+=i;
+
+printf("Sum= %d\n", sum);
+    return 0;
+}
