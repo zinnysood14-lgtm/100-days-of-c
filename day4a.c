617113776277c8907352b807f4ee5aa0b951a307
zinnysood14-lgtm <zinnysood14@gmail.com>
2026-08-20 00:35:31 +0530

    Add files via upload

---
+++ day4a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping:\n");
+    printf("a = %d\nb = %d", a, b);
+    return 0;
+}
