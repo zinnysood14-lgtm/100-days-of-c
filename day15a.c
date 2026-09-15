8ed91d8318cf5b499ff6d1f57dbc318894e4f244
Zaina <zinnysood14@gmail.com>
2026-09-15 23:14:46 +0530

    Add files via upload

---
+++ day15a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    printf("Factorial = %lld", fact);
+
+    return 0;
+}
